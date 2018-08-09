e28088f28339511951145955125e2caf80623eef
DESKTOP-AO8NR6V\Kolja <moceev@ukr.net>
2018-08-09 20:51:50 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+import {TimelineMax} from 'gsap';
+
+
+let tl = new TimelineMax();
+
+tl 
+  .from($('.logo__u'), 2,{
+    y: -30
+  })
+  .from($('.logo__r'), 2,{
+    rotation:  18,
+    transformOrigin: '100% 50%'
+  },'-=1.5')
+  .from($('.logo__i'), 2,{
+    y: '100%'
+  },'-=1.5')
+  .from($('.logo__t-top'), 3,{
+    x: '100%'
+  },'-=0.5')
+  .from($('.logo__t-bottom'), 2,{
+    y: '100%'
+  },'-=3.5')
+  .from($('.logo__y'), 2,{
+    y: '100%'
+  },'-=3.5')
+;
